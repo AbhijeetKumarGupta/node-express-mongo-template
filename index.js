535b7dd948be4ddea7a77aff71f0c0fd930a43c1
AbhijeetKumarGupta <abhijeetg375@gmail.com>
2023-11-12 12:08:45 +0530

    routes added

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
+// Imports
 require('dotenv').config();
-
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes/routes');
 const mongoString = process.env.DATABASE_URL;
 
+// DB connection and error handling
 mongoose.connect(mongoString);
 const database = mongoose.connection;
-
 database.on('error', (error) => {
     console.log(error)
 })
-
 database.once('connected', () => {
     console.log('Database Connected');
 })
-const app = express();
-app.use('/api', routes);
-app.use(express.json());
 
+// Adding middlewares (Sequence is important)
+// Note : With Express 4 we dont need body parser
+const app = express();
+app.use(express.json());
+app.use(
+    express.urlencoded({
+      extended: true,
+    }),
+);
+app.use('/api', routes);
+
+// Start Server
 app.listen(3000, () => {
     console.log(`Server Started at ${3000}`)
 })
